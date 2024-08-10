a62c7c7c450c3db5b3c426096267356c72153883
Aly Mohamed <aly.mohamed.3313@gmail.com>
2024-08-11 02:39:12 +0300

    fetch user specific images + use Next Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
-import { getImages } from "~/server/data/images";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { getUserImages } from "~/server/data/images";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const images = await getImages();
+  const user = auth();
+
+  if (!user.userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const images = await getUserImages(user.userId);
 
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-row flex-wrap justify-center gap-4">
         {images.map((image) => (
           <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} alt={image.name} />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={image.url}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
             <div>{image.name}</div>
           </div>
         ))}
--- src/server/data/images.ts
+++ src/server/data/images.ts
@@ -1,11 +1,14 @@
+import 'server-only';
+
 import { db } from "../db";
 
-export async function getImages() {
+export async function getUserImages(userId: string) {
 
     const images = await db
         .query
         .images
         .findMany({
+            where: (model, { eq }) => eq(model.userId, userId),
             orderBy: (model, { desc }) => desc(model.id)
         });
 
